27f35c42c58f995b5751887c6c365055f325937f
Benjamin Kwame Oduro <iambenqazy@gmail.com>
2023-11-03 00:13:59 +0000

    code update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the sum of 1 and 2"""
-    from add_0 import add
+# Import the add function from add_0.py
+from add_0 import add
 
-    a = 1
-    b = 2
-    total = add(a, b)
-    print("{} + {} = {}".format(a, b, total))
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Calculate the result using the add function
+result = add(a, b)
+
+# Print the result with string formatting
+print("{} + {} = {}".format(a, b, result))
 
